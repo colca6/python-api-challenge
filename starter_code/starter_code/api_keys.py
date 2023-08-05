bebaae81796faea4e602b598277159434867b3a1
Carl Colburn <carlcolburn@gmail.com>
2023-08-05 11:02:29 -0700

    commit changed API key

--- starter_code/starter_code/api_keys.py
+++ starter_code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "558b711e7503c431a45ae8f33f9c8620"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2d4415198d124ce7b8ea10eceb4ee88b"
